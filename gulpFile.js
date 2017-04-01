5c0bfe33ddcfe033358043285adcb415020dea63
liyanghua <liyanghua@baidu.com>
2017-04-01 23:08:10 +0800

    for merge

--- gulpFile.js
+++ gulpFile.js
@@ -20,21 +20,21 @@ gulp.task("scss", function() {
     return gulp.src("./css/*.scss")
         .pipe(autoprefixer({
             browsers: [
-            'last 5 iOS versions',
-            'last 5 Android versions',
-            'last 5 ExplorerMobile versions',
-            'last 5 ChromeAndroid versions',
-            'last 5 UCAndroid versions',
-            'last 5 FirefoxAndroid versions',
-            'last 5 OperaMobile versions',
-            'last 5 OperaMini versions',
-            'last 5 Samsung versions',
+                'last 5 iOS versions',
+                'last 5 Android versions',
+                'last 5 ExplorerMobile versions',
+                'last 5 ChromeAndroid versions',
+                'last 5 UCAndroid versions',
+                'last 5 FirefoxAndroid versions',
+                'last 5 OperaMobile versions',
+                'last 5 OperaMini versions',
+                'last 5 Samsung versions',
 
-            'last 3 Chrome versions',
-            'last 3 Firefox versions',
-            'last 3 Safari versions',
-            'last 3 Edge versions',
-        ],
+                'last 3 Chrome versions',
+                'last 3 Firefox versions',
+                'last 3 Safari versions',
+                'last 3 Edge versions',
+            ],
             cascade: true, //是否美化属性值 默认：true 像这样：  //-webkit-transform: rotate(45deg);  // transform: rotate(45deg);  remove:true //是否去掉不必要的前缀 默认：true
         }))
         .pipe(sass({outputStyle: 'compact'}).on("error", sass.logError))
