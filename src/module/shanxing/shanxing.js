f986a2ea5e14e3c5045a406b8e7a76904c2992ed
hemiao.he <hemiao.he@qunar.com>
2017-06-28 01:05:47 +0800

    add two pages

---
+++ src/module/shanxing/shanxing.js
@@ -0,0 +1,8 @@
+import "css/reset.css";
+import "css/basic.scss";
+import "css/m-sxintro.scss";
+import "css/m-step.scss";
+import "css/m-intro.scss";
+import "css/m-rank.scss";
+import "css/m-ads.scss";
+import "css/m-fads.scss";
