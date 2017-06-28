711764b943e4caadb2c68408d2d0c85fb06df052
hemiao.he <hemiao.he@qunar.com>
2017-06-28 13:18:46 +0800

    add

--- src/module/partner/partner.js
+++ src/module/partner/partner.js
@@ -1,3 +1,35 @@
 import "css/reset.css";
 import "css/basic.scss";
 import "./css/m-map.scss";
+
+var myChart = echarts.init(document.getElementById('j_map'));
+
+// 指定图表的配置项和数据
+var option = {
+    tooltip: {
+        trigger: 'item',
+        formatter: '{b}'
+    },
+    series: [
+        {
+            name: '中国',
+            type: 'map',
+            mapType: 'china',
+            selectedMode : 'multiple',
+            label: {
+                normal: {
+                    show: true
+                },
+                emphasis: {
+                    show: true
+                }
+            },
+            data:[
+                {name:'广东', selected:true}
+            ]
+        }
+    ]
+};
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);
