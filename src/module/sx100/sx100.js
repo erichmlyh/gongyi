4952b51694c71c511180b2863af8c10387c52e16
hemiao.he <hemiao.he@qunar.com>
2017-07-10 14:35:33 +0800

    shanxing

--- src/module/sx100/sx100.js
+++ src/module/sx100/sx100.js
@@ -1,9 +1,10 @@
 import "css/reset.css";
 import "css/basic.scss";
-import "css/m-container.scss";
+import "./css/m-container.scss";
 import "./css/m-banner.scss";
 import "./css/m-intro.scss";
 import "./css/m-fads.scss";
 import "./css/m-score.scss";
 import "./css/m-ques.scss";
 import "./css/m-shetuan.scss";
+import "./css/m-tabs.scss";
