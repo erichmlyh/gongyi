c83c24712f4135545cdc6ae620aeb46ef7d4ae8b
erichmly <erichmly@163.com>
2017-07-14 19:07:51 +0800

    adetail

--- src/module/apply_detail/adetail.js
+++ src/module/apply_detail/adetail.js
@@ -1,8 +1,8 @@
 import "css/reset.css";
 import "css/basic.scss";
 import "css/m-container.scss";
-import "css/m-sider.scss";
 import "css/m-links.scss";
 import "./css/m-activity.scss";
-import "./css/m-news.scss";
 import "./css/m-download.scss";
+
+import "./css/m-adetail.scss";
