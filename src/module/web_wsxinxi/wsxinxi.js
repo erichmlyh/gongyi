75e14b3b48323cdec25ffdf614204b6ec58a01be
erichmly <erichmly@163.com>
2017-07-15 18:30:51 +0800

    citys

--- src/module/web_wsxinxi/wsxinxi.js
+++ src/module/web_wsxinxi/wsxinxi.js
@@ -6,3 +6,27 @@ import "css/w-banner.scss";
 import "./css/w-wsxinxi.scss";
 
 import "common/js/validate.js"
+
+// 渲染省份
+var provincesHtml = "";
+$.each(citys, function(idx, province) {
+    provincesHtml += '<option value="' + province.provinceName + '">'+ province.provinceName + '</option>'
+});
+$(".province").html(provincesHtml);
+
+// 渲染城市
+renderCity(0);
+
+// 监听省份变化
+$(".province").on("change", function() {
+    var index = $(".province")[0].selectedIndex;
+    renderCity(index);
+});
+
+function renderCity(index) {
+    var citysHtml = "";
+    $.each(citys[index].citys, function(idx, city) {
+        citysHtml += '<option value="' + city.citysName + '">'+ city.citysName + '</option>'
+    });
+    $(".city").html(citysHtml);
+}
