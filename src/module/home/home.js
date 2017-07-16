5d4856735b9252701a9a5964276f724ac3aa5113
erichmly <erichmly@163.com>
2017-07-16 11:43:40 +0800

    home

--- src/module/home/home.js
+++ src/module/home/home.js
@@ -1,8 +1,21 @@
 import "css/reset.css";
 import "css/basic.scss";
-import "css/m-banner.scss";
 import "css/m-intro.scss";
 import "css/m-news.scss";
 import "css/m-picboard.scss";
 import "./css/m-proboard.scss";
 import "css/m-ads.scss";
+
+import "./css/m-banner.scss";
+
+var swiper = new Swiper('.swiper-container', {
+        // pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false,
+        loop: true
+    });
