beeea4af7e3ec1b668edbf237e2a2187628ddbcc
erichmly <erichmly@163.com>
2017-07-17 23:32:57 +0800

    个人中心

---
+++ src/mock/user_message.js
@@ -0,0 +1,33 @@
+module.exports = function (query) {
+    var data = {
+        html: 
+            `<li class="each-news">
+                <p class="title">站内公告${query.curr}</p>
+                <p class="content">【华为宣布荣耀9将限时独占《刺客信条：大革命》手游6个月】华为荣耀手机的法国官推近日发推表示，《刺客信条：大革命》手游将在荣耀9手机上限时独占6个月，IGN法国也对此消息做出了报道，但目前尚不清楚这款手游的具体发售日。</p>
+                <a href="javascript: void 0;" class="more">查看详情>></a>
+            </li>
+            <li class="each-news">
+                <p class="title">站内公告${query.curr}</p>
+                <p class="content">【华为宣布荣耀9将限时独占《刺客信条：大革命》手游6个月】华为荣耀手机的法国官推近日发推表示，《刺客信条：大革命》手游将在荣耀9手机上限时独占6个月，IGN法国也对此消息做出了报道，但目前尚不清楚这款手游的具体发售日。</p>
+                <a href="javascript: void 0;" class="more">查看详情>></a>
+            </li>
+            <li class="each-news">
+                <p class="title">站内公告${query.curr}</p>
+                <p class="content">【华为宣布荣耀9将限时独占《刺客信条：大革命》手游6个月】华为荣耀手机的法国官推近日发推表示，《刺客信条：大革命》手游将在荣耀9手机上限时独占6个月，IGN法国也对此消息做出了报道，但目前尚不清楚这款手游的具体发售日。</p>
+                <a href="javascript: void 0;" class="more">查看详情>></a>
+            </li>
+            <li class="each-news">
+                <p class="title">站内公告${query.curr}</p>
+                <p class="content">【华为宣布荣耀9将限时独占《刺客信条：大革命》手游6个月】华为荣耀手机的法国官推近日发推表示，《刺客信条：大革命》手游将在荣耀9手机上限时独占6个月，IGN法国也对此消息做出了报道，但目前尚不清楚这款手游的具体发售日。</p>
+                <a href="javascript: void 0;" class="more">查看详情>></a>
+            </li>`
+    };
+
+    return {
+        "errno": 0,
+        "errmsg": "",
+        "data": data
+    }
+}
+
+
--- src/module/user_message/message.js
+++ src/module/user_message/message.js
@@ -2,3 +2,5 @@ import "css/reset.css";
 import "css/basic.scss";
 
 import "./css/m-message.scss";
+
+import "js/pagenation.js"
