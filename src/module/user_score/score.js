38fb72d19d27ed1c7e892ceea74ea4f6fbe6874e
erichmly <erichmly@163.com>
2017-07-18 01:03:43 +0800

    成绩查询

--- src/module/user_score/score.js
+++ src/module/user_score/score.js
@@ -9,6 +9,7 @@ var url = $htmlContainer.data("url");
 $("#search").on("click", function() {
     var name = $("#name").val();
     var type = $("#type").val();
+    var date = $("#date").val();
     if(!name) {
         layer.open({
             title: 'tips:',
@@ -23,6 +24,7 @@ $("#search").on("click", function() {
         data: {
             name: name,
             type: type,
+            date: date
         },
         success: function (data) {
             data = data && data.data || {};
@@ -33,4 +35,28 @@ $("#search").on("click", function() {
         error: function () {
         }
     })
+});
+
+// 日历框
+layui.use('laydate', function () {
+    var laydate = layui.laydate;
+
+    var start = {
+        min: laydate.now()
+        , max: '2099-06-16 23:59:59'
+        , istoday: false
+        , choose: function (datas) {
+            end.min = datas; //开始日选好后，重置结束日的最小日期
+            end.start = datas //将结束日的初始值设定为开始日
+        }
+    };
+
+    var end = {
+        min: laydate.now()
+        , max: '2099-06-16 23:59:59'
+        , istoday: false
+        , choose: function (datas) {
+            start.max = datas; //结束日选好后，重置开始日的最大日期
+        }
+    };
 });
