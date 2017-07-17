f2aa0c924273cd91a524888b53d3fb51c48e2cdb
erichmly <erichmly@163.com>
2017-07-17 13:43:36 +0800

    pages

---
+++ src/mock/download.js
@@ -0,0 +1,63 @@
+module.exports = function (query) {
+    var data = {
+        html: 
+            `<tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>
+            <tr>
+                <td>扶贫资料${query.curr}</td>
+                <td>2017-05-06</td>
+                <td><a href="/xx/xx/xx" download="">下载</a></td>
+            </tr>`
+    };
+
+    return {
+        "errno": 0,
+        "errmsg": "",
+        "data": data
+    }
+}
+
+
--- src/module/about_download/download.js
+++ src/module/about_download/download.js
@@ -1,5 +1,40 @@
 import "css/reset.css";
 import "css/basic.scss";
 import "css/m-table.scss";
-import "css/m-pagenation.scss";
 import "./css/m-download.scss";
+import "./css/m-pagenation.scss";
+
+var $htmlContainer = $("#html-container");
+var url = $htmlContainer.data("url");
+var pages = $htmlContainer.data("pages");
+
+layui.use(['laypage', 'layer'], function () {
+    var laypage = layui.laypage;
+    var layer = layui.layer;
+    laypage({
+        cont: 'J-pagenation',
+        pages: parseInt(pages) || 1,
+        skin: '#f76d02',
+        jump: function (obj) {
+            var curPage = obj.curr || 1;
+            // 请求数据
+            $.ajax({
+                url: url,
+                type: "GET",
+                dataType: "json",
+                data: { curr : curPage },
+                success: function (data) {
+                    data = data && data.data || {};
+                    if (data.html) {
+                        $htmlContainer.html(data.html);
+                    }
+                },
+                error: function () {
+                }
+            })
+        }
+    });
+});
+
+
+
