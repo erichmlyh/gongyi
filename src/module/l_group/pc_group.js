26b28101a3e71a708538205cee03af5ec23eaa7a
erichmly <erichmly@163.com>
2017-07-17 21:02:28 +0800

    l

--- src/module/l_group/pc_group.js
+++ src/module/l_group/pc_group.js
@@ -2,6 +2,7 @@ import "css/reset.css";
 import "css/basic.scss";
 import "./css/m-group.scss";
 
+// 上传文件
 layui.upload({
   url: '../mock/upload',
   title: '上传文件',
@@ -10,3 +11,27 @@ layui.upload({
     $("#upload-show").val(data.url);
   }
 });
+
+// 渲染省份
+var provincesHtml = "";
+$.each(citys, function(idx, province) {
+    provincesHtml += '<option value="' + province.provinceName + '">'+ province.provinceName + '</option>'
+});
+$(".province").html(provincesHtml);
+
+// 渲染城市
+renderCity(0);
+
+// 监听省份变化
+$(".province").on("change", function() {
+    var index = $(".province")[0].selectedIndex;
+    renderCity(index);
+});
+
+function renderCity(index) {
+    var citysHtml = "";
+    $.each(citys[index].citys, function(idx, city) {
+        citysHtml += '<option value="' + city.citysName + '">'+ city.citysName + '</option>'
+    });
+    $(".city").html(citysHtml);
+}
--- src/module/l_signdetail/pc_signdetail.js
+++ src/module/l_signdetail/pc_signdetail.js
@@ -1,3 +1,27 @@
 import "css/reset.css";
 import "css/basic.scss";
 import "./css/m-signdetail.scss";
+
+// 渲染省份
+var provincesHtml = "";
+$.each(citys, function(idx, province) {
+    provincesHtml += '<option value="' + province.provinceName + '">'+ province.provinceName + '</option>'
+});
+$(".province").html(provincesHtml);
+
+// 渲染城市
+renderCity(0);
+
+// 监听省份变化
+$(".province").on("change", function() {
+    var index = $(".province")[0].selectedIndex;
+    renderCity(index);
+});
+
+function renderCity(index) {
+    var citysHtml = "";
+    $.each(citys[index].citys, function(idx, city) {
+        citysHtml += '<option value="' + city.citysName + '">'+ city.citysName + '</option>'
+    });
+    $(".city").html(citysHtml);
+}
