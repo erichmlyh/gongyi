12e5ee13ceb9aa7ab3f3c8c7fe8f70bfa5fc461e
erichmly <erichmly@163.com>
2017-07-18 00:03:00 +0800

    成绩录入

--- src/module/user_record/record.js
+++ src/module/user_record/record.js
@@ -2,3 +2,31 @@ import "css/reset.css";
 import "css/basic.scss";
 import "css/m-tb.scss";
 import "css/m-forme.scss";
+
+import "./css/m-date.scss";
+
+import "js/pc_validate.js"
+
+// 日历框
+layui.use('laydate', function () {
+    var laydate = layui.laydate;
+
+    var start = {
+        min: laydate.now()
+        , max: '2099-06-16 23:59:59'
+        , istoday: false
+        , choose: function (datas) {
+            end.min = datas; //开始日选好后，重置结束日的最小日期
+            end.start = datas //将结束日的初始值设定为开始日
+        }
+    };
+
+    var end = {
+        min: laydate.now()
+        , max: '2099-06-16 23:59:59'
+        , istoday: false
+        , choose: function (datas) {
+            start.max = datas; //结束日选好后，重置开始日的最大日期
+        }
+    };
+});
