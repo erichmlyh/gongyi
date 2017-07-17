f6fd7d3a7953231a9617c9e50e85e1b2ad6a09e2
erichmly <erichmly@163.com>
2017-07-18 01:01:24 +0800

    成绩查询

---
+++ src/mock/user_score.js
@@ -0,0 +1,31 @@
+module.exports = function (query) {
+    var data = {
+        html: 
+            `<tr>
+                <td>${query.name}</td>
+                <td>北京交通大学${query.type}${query.date}</td>
+                <td>12小时</td>
+                <td>250元</td>
+            </tr>
+            <tr>
+                <td>${query.name}</td>
+                <td>北京交通大学${query.type}${query.date}</td>
+                <td>12小时</td>
+                <td>250元</td>
+            </tr>
+            <tr>
+                <td>${query.name}</td>
+                <td>北京交通大学${query.type}${query.date}</td>
+                <td>12小时</td>
+                <td>250元</td>
+            </tr>`
+    };
+
+    return {
+        "errno": 0,
+        "errmsg": "",
+        "data": data
+    }
+}
+
+
