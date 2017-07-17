bf4f1829bf98f8bb7744cc92c7d9b5cc86c39a39
erichmly <erichmly@163.com>
2017-07-17 16:20:12 +0800

    sx100

--- src/module/sx100/sx100.js
+++ src/module/sx100/sx100.js
@@ -9,3 +9,29 @@ import "./css/m-score.scss";
 import "./css/m-ques.scss";
 import "./css/m-shetuan.scss";
 import "./css/m-tabs.scss";
+
+// 个人成绩
+$("#J-person-all").hide();
+$(".m-tabs").find(".tabs").on("click", ".J-tit-person", function(e){
+    var $this = $(this);
+    var me = $this.data("me");
+    var all = $this.data("all");
+
+    $(".m-tabs").find(".J-tit-person").removeClass("cur");
+    $this.addClass("cur");
+    $("." + all).hide();
+    $("#" + me).show();
+});
+
+// 社团成绩
+$("#J-recruit-all").hide();
+$(".m-tabs").find(".tabs").on("click", ".J-tit-recruit", function(e){
+    var $this = $(this);
+    var me = $this.data("me");
+    var all = $this.data("all");
+
+    $(".m-tabs").find(".J-tit-recruit").removeClass("cur");
+    $this.addClass("cur");
+    $("." + all).hide();
+    $("#" + me).show();
+});
