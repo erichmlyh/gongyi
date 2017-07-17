d6b68ae98406c7048c911ea802ea8d295c7183e6
erichmly <erichmly@163.com>
2017-07-17 14:53:27 +0800

    pages

---
+++ src/mock/newslist.js
@@ -0,0 +1,46 @@
+module.exports = function (query) {
+    var data = {
+        html: 
+            `<li class="itm">
+                <img src="/static/img/newscd2d04f3f83287ff7b0d571b567c03da.jpg" alt="">
+                <div class="dtl">
+                    <h2 class="tit">刘泽彭理事长走进粤北地区，带你看支教课堂！第${query.curr}页</h2>
+                    <p class="abs">在美丽中国理事长刘泽彭的带领下，深圳多位优秀企业家、当地政府领导也来到美丽中国在河源地区的项目学校，看望和鼓励支教老师们..</p>
+                    <div>
+                        <span class="ifo">发布时间：2017-06-16</span>
+                        <a href="/xx/xx" class="btn">查看</a>
+                    </div>
+                </div>
+            </li>
+            <li class="itm">
+                <img src="/static/img/newscd2d04f3f83287ff7b0d571b567c03da.jpg" alt="">
+                <div class="dtl">
+                    <h2 class="tit">刘泽彭理事长走进粤北地区，带你看支教课堂！第${query.curr}页</h2>
+                    <p class="abs">在美丽中国理事长刘泽彭的带领下，深圳多位优秀企业家、当地政府领导也来到美丽中国在河源地区的项目学校，看望和鼓励支教老师们..</p>
+                    <div>
+                        <span class="ifo">发布时间：2017-06-16</span>
+                        <a href="/xx/xx" class="btn">查看</a>
+                    </div>
+                </div>
+            </li>
+            <li class="itm">
+                <img src="/static/img/newscd2d04f3f83287ff7b0d571b567c03da.jpg" alt="">
+                <div class="dtl">
+                    <h2 class="tit">刘泽彭理事长走进粤北地区，带你看支教课堂！第${query.curr}页</h2>
+                    <p class="abs">在美丽中国理事长刘泽彭的带领下，深圳多位优秀企业家、当地政府领导也来到美丽中国在河源地区的项目学校，看望和鼓励支教老师们..</p>
+                    <div>
+                        <span class="ifo">发布时间：2017-06-16</span>
+                        <a href="/xx/xx" class="btn">查看</a>
+                    </div>
+                </div>
+            </li>`
+    };
+
+    return {
+        "errno": 0,
+        "errmsg": "",
+        "data": data
+    }
+}
+
+
--- src/module/home_newslist/newslist.js
+++ src/module/home_newslist/newslist.js
@@ -3,4 +3,6 @@ import "css/basic.scss";
 import "css/m-container.scss";
 import "css/m-sider.scss";
 import "css/m-list.scss";
-import "css/m-pagenation.scss";
+import "./css/m-pagenation.scss";
+
+import "js/pagenation.js";
