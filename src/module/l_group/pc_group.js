fa058fd860202747c5f7a787b4ffbc8da7aaca4b
hemiao.he <hemiao.he@qunar.com>
2017-07-17 18:41:54 +0800

    push

--- src/module/l_group/pc_group.js
+++ src/module/l_group/pc_group.js
@@ -1,3 +1,10 @@
 import "css/reset.css";
 import "css/basic.scss";
 import "./css/m-group.scss";
+
+layui.upload({
+  url: '/xxx/xx',
+  success: function(res){
+    console.log(res); //上传成功返回值，必须为json格式
+  }
+});
