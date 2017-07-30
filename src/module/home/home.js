84d67105d3378e8a48d40ab0d36292065a603293
erichmly <erichmly@163.com>
2017-07-30 12:49:09 +0800

    yes

--- src/module/home/home.js
+++ src/module/home/home.js
@@ -3,6 +3,9 @@ import "css/basic.scss";
 import "css/m-intro.scss";
 import "css/m-news.scss";
 import "css/m-picboard.scss";
+import "css/m-sarrow.scss";
+
+
 import "./css/m-proboard.scss";
 import "./css/m-ads.scss";
 
